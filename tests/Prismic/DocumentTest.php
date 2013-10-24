55b5498a7396dca38cc57306f758d5210f54be10
Adam Engebretson <adam@enge.me>
2013-10-24 11:48:55 -0500

    Add DocumentTest case for __get() and __toString() methods.
    Signed-off-by: Adam Engebretson <adam@enge.me>

--- tests/Prismic/DocumentTest.php
+++ tests/Prismic/DocumentTest.php
@@ -99,4 +99,18 @@ class DocumentTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($this->document->getHtml('product.birthdate'), '<time>2013-10-23</time>');
         //TODO
     }
+
+    public function testToString()
+    {
+        $this->assertEquals((string)$this->document, 'UjHYesuvzT0A_yi6');
+    }
+
+    public function testDynamicGet()
+    {
+        $this->assertEquals($this->document->name, 'Cool Coconut Macaron');
+        $this->assertEquals((string)$this->document->price, 2.5);
+        $this->assertEquals((string)$this->document->adult, 'yes');
+        $this->assertEquals((string)$this->document->birthdate, '2013-10-23');
+        $this->assertTrue(null != $this->document->image);
+    }
 }
