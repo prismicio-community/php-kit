6bc96c8c778d0b862432736f1476fa09012b12d7
Rudy Rigot <rudy.rigot@gmail.com>
2014-06-18 15:59:32 -0700

    Documentation of some fragments

--- src/Prismic/Fragment/Color.php
+++ src/Prismic/Fragment/Color.php
@@ -1,6 +1,5 @@
 <?php
-
-/*
+/**
  * This file is part of the Prismic PHP SDK
  *
  * Copyright 2013 Zengularity (http://www.zengularity.com).
@@ -11,25 +10,61 @@
 
 namespace Prismic\Fragment;
 
+/**
+ * This class embodies a Color fragment.
+ *
+ * @api
+ */
 class Color implements FragmentInterface
 {
+    /**
+     * @var string the hexadecimal code of the color
+     */
     private $hex;
 
+    /**
+     * Constructs a Color fragment.
+     *
+     * @param string  $hex  the hexadecimal code of the color
+     */
     public function __construct($hex)
     {
         $this->hex = $hex;
     }
 
+    /**
+     * Builds a HTML version of the color.
+     *
+     * @api
+     *
+     * @param \Prismic\LinkResolver $linkResolver the link resolver
+     *
+     * @return string the HTML version of the color
+     */
     public function asHtml($linkResolver = null)
     {
         return '<span class="color">' . htmlentities($this->hex) . '</span>';
     }
 
+    /**
+     * Builds a text version of the color.
+     *
+     * @api
+     *
+     * @return string the text version of the color
+     */
     public function asText()
     {
         return $this->getHexValue();
     }
 
+    /**
+     * Returns the hexadecimal code of the color.
+     *
+     * @api
+     *
+     * @return string the hexadecimal code of the color
+     */
     public function getHexValue()
     {
         return $this->hex;
--- src/Prismic/Fragment/Date.php
+++ src/Prismic/Fragment/Date.php
@@ -1,6 +1,5 @@
 <?php
-
-/*
+/**
  * This file is part of the Prismic PHP SDK
  *
  * Copyright 2013 Zengularity (http://www.zengularity.com).
@@ -11,35 +10,85 @@
 
 namespace Prismic\Fragment;
 
+/**
+ * This class embodies a date fragment.
+ */
 class Date implements FragmentInterface
 {
+    /**
+     * @var string the date's value
+     */
     private $value;
 
+    /**
+     * Constructs a Date fragment.
+     *
+     * @param string  $value  the date's value
+     */
     public function __construct($value)
     {
         $this->value = $value;
     }
 
+    /**
+     * Builds a HTML version of the date.
+     *
+     * @api
+     *
+     * @param \Prismic\LinkResolver $linkResolver the link resolver
+     *
+     * @return string the HTML version of the date
+     */
     public function asHtml($linkResolver = null)
     {
         return '<time>' . htmlentities($this->value) . '</time>';
     }
 
+    /**
+     * Builds a text version of the date.
+     *
+     * @api
+     *
+     * @return string the text version of the date
+     */
     public function asText()
     {
         return $this->value;
     }
 
+    /**
+     * Returns the date following a certain pattern.
+     *
+     * @param string  $pattern  the pattern, as expected by the date function in PHP
+     *
+     * @api
+     *
+     * @return string the date, formatted
+     */
     public function formatted($pattern)
     {
         return date($pattern, $this->asEpoch());
     }
 
+    /**
+     * Returns the date as an epoch date.
+     *
+     * @api
+     *
+     * @return string the date as an epoch date
+     */
     public function asEpoch()
     {
         return strtotime($this->value);
     }
 
+    /**
+     * Returns the date's value.
+     *
+     * @api
+     *
+     * @return string the date's value
+     */
     public function getValue()
     {
         return $this->value;
