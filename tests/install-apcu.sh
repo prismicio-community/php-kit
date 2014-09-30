96745f0bdae73a69b1fdaaec4dc2a83ac158f90f
Erwan Loisant <eloisant@gmail.com>
2014-09-30 16:42:13 +0200

    Use APCu 4.0.6

--- tests/install-apcu.sh
+++ tests/install-apcu.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_PHP_VERSION" == "5.5" ] || [ "$TRAVIS_PHP_VERSION" == "5.6" ] ; then
     sudo apt-get install autoconf
-    APCU=4.0.2
+    APCU=4.0.6
     wget http://pecl.php.net/get/apcu-$APCU.tgz
     tar zxvf apcu-$APCU.tgz
     cd "apcu-${APCU}"
