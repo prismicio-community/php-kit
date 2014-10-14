58a7f345d0f6930faf19acec5eeeee9cc722b1bb
Erwan Loisant <eloisant@gmail.com>
2014-10-14 10:35:18 +0200

    Version 1.0.10

--- src/Prismic/Api.php
+++ src/Prismic/Api.php
@@ -25,7 +25,7 @@ use \Prismic\Cache\DefaultCache;
 class Api
 {
 
-    const VERSION = "1.0.9";
+    const VERSION = "1.0.10";
 
     /**
      * @var string the API's access token to be used with each API call
