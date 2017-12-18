66df31e4b802a77d8bd64070921ba58f3cfc45ad
jbdebiasio <jbaptistedebiasio@gmail.com>
2017-12-18 13:18:54 +0100

    add tests on Date and Link

--- tests/Prismic/FakeLinkResolver.php
+++ tests/Prismic/FakeLinkResolver.php
@@ -2,21 +2,16 @@
 
 namespace Prismic\Test;
 
-use Prismic\Fragment\Link\DocumentLink;
 use Prismic\LinkResolver;
 
 class FakeLinkResolver extends LinkResolver
 {
-    /**
-     * @param DocumentLink $link
-     *
-     * @return null|string
-     */
     public function resolve($link)
     {
-        if ($link->isBroken()) {
-            return null;
+        if ($link->isBroken) {
+            return 'http://host/404';
         }
-        return "http://host/doc/".$link->getId();
+
+        return 'http://host/doc/'.$link->id;
     }
 }
