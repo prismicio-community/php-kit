bfb8b0623b88ddf68f18be16ebd1db0e3b0af708
jbdebiasio <jbaptistedebiasio@gmail.com>
2017-12-19 12:39:41 +0100

    add test html serializer

--- tests/Prismic/Dom/RichTextTest.php
+++ tests/Prismic/Dom/RichTextTest.php
@@ -28,7 +28,7 @@ class RichTextTest extends \PHPUnit_Framework_TestCase
         $expected = (
             '<h1>Heading 1</h1>' .
             '<h2>Heading 2</h2>' .
-            '<h3>Heading 3</h3>' .
+            '<h3 class="the-label">Heading 3</h3>' .
             '<h4>Heading 4</h4>' .
             '<h5>Heading 5</h5>' .
             '<h6>Heading 6</h6>' .
@@ -55,6 +55,57 @@ class RichTextTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertEquals($expected, RichText::asHtml($this->richText->description, $this->linkResolver));
         $this->assertEquals($expected, RichText::asHtml($this->richText->description));
+    }
+
+    public function testHtmlSerializer()
+    {
+        $expected = (
+            '<h1>Heading 1</h1>' .
+            '<h2>Heading 2</h2>' .
+            '<div><h3 class="custom the-label">Heading 3</h3></div>' .
+            '<h4>Heading 4</h4>' .
+            '<h5>Heading 5</h5>' .
+            '<h6>Heading 6</h6>' .
+            '<p>Paragraph <em>em</em> and <span class="custom">strong</span></p>' .
+            '<p>Paragraph <a target="_blank" rel="noopener" href="https://prismic.io">Web link</a> and <a href="https://prismic-io.s3.amazonaws.com/levi-templeting%2F6acedd00-083a-4335-b789-1ccb64b37ead_success-kid-speak-english.jpg">media link</a></p>' .
+            '<pre>Preformatted block</pre>' .
+            '<ul>' .
+                '<li><em>Help</em></li>' .
+                '<li>Revolver</li>' .
+                '<li>Abbey Road</li>' .
+            '</ul>' .
+            '<ol>' .
+                '<li><a href="https://prismic.io">John</a></li>' .
+                '<li>Paul</li>' .
+                '<li>George</li>' .
+                '<li>Ringo</li>' .
+            '</ol>' .
+            '<p class="block-img">' .
+                '<img src="https://prismic-io.s3.amazonaws.com/levi-templeting/357366ce9af5fd05dcd0a76e6ee267fc46c08f6a_mi0003995354.jpg" alt="Alt text">' .
+            '</p>' .
+            '<div data-oembed="https://www.youtube.com/watch?v=joA7VpZLQaQ" data-oembed-type="video" data-oembed-provider="youtube">' .
+                '<iframe width="480" height="270" src="https://www.youtube.com/embed/joA7VpZLQaQ?feature=oembed" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>' .
+            '</div>'
+        );
+        $htmlSerializer = function($element, $content) {
+            if ($element->type === 'heading3') {
+                $classes = 'custom';
+                if (property_exists($element, 'label')) {
+                    $classes .= ' ' . $element->label;
+                }
+                return '<div><h3 class="' . $classes . '">' . $content . '</h3></div>';
+            }
+            if ($element->type === 'strong') {
+                $classes = 'custom';
+                if (property_exists($element, 'label')) {
+                    $classes .= ' ' . $element->label;
+                }
+                return '<span class="' . $classes . '">' . $content . '</span>';
+            }
+            return null;
+        };
+        $actual = RichText::asHtml($this->richText->description, $this->linkResolver, $htmlSerializer);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testDocumentLink()
