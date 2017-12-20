59600c783cf8e4309b88c686b910a66632487982
jbdebiasio <jbaptistedebiasio@gmail.com>
2017-12-20 16:08:36 +0100

    update tests Date

--- tests/Prismic/Dom/DateTest.php
+++ tests/Prismic/Dom/DateTest.php
@@ -4,13 +4,14 @@ namespace Prismic\Test;
 
 use Prismic\Dom\Date;
 use \DateTime;
+use \DateTimeZone;
 
 class DateTest extends \PHPUnit_Framework_TestCase
 {
     public function testDateAsDate()
     {
         $date = '2017-02-17';
-        $expected = new DateTime($date);
+        $expected = new DateTime($date, new DateTimeZone('UTC'));
         $actual = Date::asDate($date);
         $this->assertEquals($expected, $actual);
     }
@@ -18,7 +19,7 @@ class DateTest extends \PHPUnit_Framework_TestCase
     public function testTimestampAsDate()
     {
         $timestamp = '2017-02-17T12:30:00+0000';
-        $expected = new DateTime($timestamp);
+        $expected = new DateTime($timestamp, new DateTimeZone('UTC'));
         $actual = Date::asDate($timestamp);
         $this->assertEquals($expected, $actual);
     }
