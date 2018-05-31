f56e3001fbdb8876511fde38f7afd47f24366b7b
George Steel <george@net-glue.co.uk>
2018-05-31 12:21:14 +0100

    Fix incorrect case for method name

--- src/Prismic/Document.php
+++ src/Prismic/Document.php
@@ -215,7 +215,7 @@ class Document implements DocumentInterface
         foreach ($this->alternateLanguages as $language) {
             if (isset($language->lang) && $language->lang === $lang) {
                 $id = isset($language->id) ? $language->id : null;
-                return $id ? $this->api->getByID($id) : null;
+                return $id ? $this->api->getById($id) : null;
             }
         }
         return null;
