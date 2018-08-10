77c9c02061e751bed4d9a277897e64fb9f32c1a4
George Steel <george@net-glue.co.uk>
2018-08-10 01:02:33 +0100

    Import functions, and check that the link span is not potentially null

--- src/Prismic/Document/Fragment/HtmlHelperTrait.php
+++ src/Prismic/Document/Fragment/HtmlHelperTrait.php
@@ -6,6 +6,16 @@ namespace Prismic\Document\Fragment;
 use Prismic\Document\Fragment\Link\AbstractLink;
 use Prismic\LinkResolver;
 use Zend\Escaper\Escaper;
+use function array_walk;
+use function implode;
+use function is_array;
+use function is_scalar;
+use function json_encode;
+use function nl2br;
+use function preg_split;
+use function sprintf;
+use function strpos;
+use function substr;
 
 trait HtmlHelperTrait
 {
@@ -18,21 +28,21 @@ trait HtmlHelperTrait
         foreach ($attributes as $key => $val) {
             $key = $this->escapeHtml($key);
 
-            if (('on' == \substr($key, 0, 2)) || ('constraints' == $key)) {
+            if (('on' == substr($key, 0, 2)) || ('constraints' == $key)) {
                 // Don't escape event attributes; _do_ substitute double quotes with singles
-                if (! \is_scalar($val)) {
+                if (! is_scalar($val)) {
                     // non-scalar data should be cast to JSON first
-                    $val = \json_encode($val);
+                    $val = json_encode($val);
                 }
             } else {
-                if (\is_array($val)) {
-                    $val = \implode(' ', $val);
+                if (is_array($val)) {
+                    $val = implode(' ', $val);
                 }
             }
 
             $val = $this->escapeHtmlAttr($val);
 
-            if (\strpos($val, '"') !== false) {
+            if (strpos($val, '"') !== false) {
                 $html .= " $key='$val'";
             } else {
                 $html .= " $key=\"$val\"";
@@ -66,8 +76,8 @@ trait HtmlHelperTrait
             return nl2br($this->escapeHtml($text));
         }
 
-        $nodes = \preg_split('//u', $text, -1, \PREG_SPLIT_NO_EMPTY);
-        \array_walk($nodes, function (&$character) {
+        $nodes = preg_split('//u', $text, -1, \PREG_SPLIT_NO_EMPTY);
+        array_walk($nodes, function (&$character) {
             $character = $this->escapeHtml($character);
         });
         foreach ($spans as $span) {
@@ -92,9 +102,14 @@ trait HtmlHelperTrait
 
                 case 'hyperlink':
                     $link = AbstractLink::abstractFactory($span->data, $linkResolver);
-                    $openTag  = $link->openTag();
-                    $closeTag = $link->closeTag();
+                    if ($link) {
+                        $openTag  = $link->openTag();
+                        $closeTag = $link->closeTag();
+                    }
                     break;
+            }
+            if (! $openTag || ! $closeTag) {
+                continue;
             }
             $nodes[$span->start] = sprintf('%s%s', $openTag, $nodes[$span->start]);
             $nodes[$end] = sprintf('%s%s', $nodes[$end], $closeTag);
