3563142af6d61c21b694850741a05337567829a9
Jeroen Boersma <jeroen@elgentos.nl>
2019-03-20 21:34:19 +0100

    Added Language reference
    + added Language
    + updated ApiData
    * updated tests

---
+++ tests/Prismic/LanguageTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 20-3-19
+ * Time: 21:24
+ */
+
+namespace Prismic\Test;
+
+
+use Prismic\Exception\InvalidArgumentException;
+use Prismic\Language;
+
+class LanguageTest extends TestCase
+{
+
+    /** @var Language */
+    private $language;
+    /** @var \stdClass */
+    private $json;
+
+    public function setUp()
+    {
+        $this->json = $json = \json_decode($this->getJsonFixture('language.json'));
+        $this->language = Language::parse($json);
+    }
+
+    public function testCorrectId()
+    {
+        $this->assertSame($this->json->id, $this->language->getId());
+        $this->assertSame($this->json->name, $this->language->getName());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testWrongObjectType()
+    {
+        Language::parse(new \stdClass);
+    }
+
+}
