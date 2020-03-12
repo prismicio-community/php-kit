a7629c02849b59b41a93537236836e73ad75e5b6
George Steel <george@net-glue.co.uk>
2020-03-12 14:28:03 +0000

    Change boolean fragment type to inherit from generic scalar fragment Add unit test for expected payload values Add boolean data type to search results fixtures in V1/V2 format

--- src/Prismic/Document/Fragment/Boolean.php
+++ src/Prismic/Document/Fragment/Boolean.php
@@ -3,78 +3,10 @@ declare(strict_types=1);
 
 namespace Prismic\Document\Fragment;
 
-use Prismic\Exception\InvalidArgumentException;
-
-class Boolean implements FragmentInterface
+class Boolean extends AbstractScalarFragment
 {
-    use HtmlHelperTrait;
-
-    /** @var mixed */
-    protected $value;
-
-    protected function __construct()
+    public function asBoolean() : bool
     {
-    }
-
-    public static function factory($value)
-    {
-        if (\is_object($value) && \property_exists($value, 'value')) {
-            $value = $value->value; // V1 API
-        }
-        if (\is_object($value) || \is_array($value)) {
-            throw new InvalidArgumentException(\sprintf(
-                'Cannot determine single boolean value from input of type %s with value %s',
-                gettype($value),
-                \json_encode($value)
-            ));
-        }
-
-        $fragment = new static();
-        $fragment->value = $value;
-
-        return $fragment;
-    }
-
-    public function asText() : ?string
-    {
-        $value = (string) ((int) ((bool) $this->value));
-
-        return ($value === '') ? null : $value;
-    }
-
-    public function asHtml() : ?string
-    {
-        $value = (string) $this->asText();
-
-        return ($value === '') ? null : $this->escapeHtml($value);
-    }
-
-    public function asHtmlAttribute() : ?string
-    {
-        $value = (string) $this->asText();
-
-        return ($value === '') ? null : $this->escapeHtmlAttr($value);
-    }
-
-    public function asInteger() : ?int
-    {
-        $value = $this->asBoolean();
-
-        if ($value === null) {
-            return null;
-        }
-
-        return (int) $value;
-    }
-
-    public function asBoolean() : ?bool
-    {
-        $value = $this->asText();
-
-        if ($value === null) {
-            return null;
-        }
-
-        return (bool) $value;
+        return (bool) $this->value;
     }
 }
---
+++ tests/Prismic/Document/Fragment/BooleanTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Prismic\Test\Document\Fragment;
+
+use Prismic\Document\Fragment\Boolean;
+use PHPUnit\Framework\TestCase;
+use function json_decode;
+
+class BooleanTest extends TestCase
+{
+    public function possibleFragmentValues() : iterable
+    {
+        return [
+            'V1 Style, True' => ['{"type": "Boolean","value": true}', true],
+            'V1 Style, False' => ['{"type": "Boolean","value": false}', false],
+            'V2 Style, True' => ['true', true],
+            'V2 Style, False' => ['false', false],
+        ];
+    }
+
+    /** @dataProvider possibleFragmentValues */
+    public function testFactoryWithV1FragmentPayload(string $json, bool $expect) : void
+    {
+        $data = json_decode($json, false);
+        $fragment = Boolean::factory($data);
+        $this->assertInstanceOf(Boolean::class, $fragment);
+        $this->assertSame($expect, $fragment->asBoolean());
+    }
+}
