5c83ae87ce35cbdb13c2762a8b8509ef0f4496eb
Jeroen Boersma <jeroen@elgentos.nl>
2022-05-16 23:15:31 +0200

    Fixed minor formating
    * needs a extra cli param `-d 'apc.enable_cli=1'`

--- tests/Prismic/CacheTest.php
+++ tests/Prismic/CacheTest.php
@@ -17,13 +17,14 @@ class CacheTest extends TestCase
     public function testSetGetValue()
     {
         $this->cache->set('key', 'value');
-        $this->assertEquals($this->cache->get('key'), 'value');
+        $this->assertEquals('value', $this->cache->get('key'));
     }
 
     public function testSetDeleteValue()
     {
         $this->cache->set('key', 'value');
-        $this->assertEquals($this->cache->get('key'), 'value');
+        $this->assertEquals('value', $this->cache->get('key'));
+
         $this->cache->delete('key');
         $this->assertNull($this->cache->get('key'));
     }
@@ -34,14 +35,17 @@ class CacheTest extends TestCase
         $this->assertTrue($this->cache->has('key'));
         $this->assertFalse($this->cache->has('key1'));
         $this->assertFalse($this->cache->has('key2'));
+
         $this->cache->set('key1', 'value1');
         $this->cache->set('key2', 'value2');
         $this->assertTrue($this->cache->has('key'));
         $this->assertTrue($this->cache->has('key1'));
         $this->assertTrue($this->cache->has('key2'));
-        $this->assertEquals($this->cache->get('key'), 'value');
-        $this->assertEquals($this->cache->get('key1'), 'value1');
-        $this->assertEquals($this->cache->get('key2'), 'value2');
+
+        $this->assertEquals('value', $this->cache->get('key'));
+        $this->assertEquals('value1', $this->cache->get('key1'));
+        $this->assertEquals('value2', $this->cache->get('key2'));
+
         $this->cache->clear();
         $this->assertFalse($this->cache->has('key'));
         $this->assertFalse($this->cache->has('key1'));
@@ -53,7 +57,8 @@ class CacheTest extends TestCase
 
     public function testSetGetReturnsExpectedValue()
     {
-        $data = \json_decode($this->getJsonFixture('data.json'));
+        $data = \json_decode($this->getJsonFixture('data.json'), false);
+
         $this->cache->set('key', $data);
         $result = $this->cache->get('key');
         $this->assertEquals($data, $result);
@@ -61,9 +66,11 @@ class CacheTest extends TestCase
 
     public function testLongUrlBasedCacheKeysArePersistedCorrectly()
     {
-        $data = \json_decode($this->getJsonFixture('data.json'));
+        $data = \json_decode($this->getJsonFixture('data.json'), false);
+
         $url = $data->forms->everything->action;
         $url .= '?access_token=AVeryLongAccessTokenForPermanentAccessToTheRepository&q=SomeQueryString';
+
         $this->cache->set($url, $data);
         $result = $this->cache->get($url);
         $this->assertEquals($data, $result);
